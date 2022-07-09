958d2a80f1ec80f7fee6dff19b1cdd9afc7f532b
sdeenaa <108716764+sdeenaa@users.noreply.github.com>
2022-07-09 02:01:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const button=document.getElementById("button")
+
+const bg= [" #ff3381" , "#93F","#f56","#eeff00","#003cFF"]
+const fg= [" #111","#fff","#111","#111","#fff"]
+
+function color(){
+    let limit=bg.length;
+    let index=Math.floor(Math.random()*limit);
+    button.style.background=bg[index];
+    button.style.color=fg[index];
+}
